2570ca9c214943ca966adc980b445f660c6f67d9
u1qns <85053365+u1qns@users.noreply.github.com>
2022-07-14 02:50:11 +0900

    Update and rename 5076_Web Page.cpp to 5076_Web Pages.cpp

---
+++ 5076_Web Pages.cpp
@@ -0,0 +1,78 @@
+
+#include <iostream>
+#include <string>
+#include <stack>
+
+int main()
+{
+    
+    std::string text;
+    while(1)
+    {
+        bool isLegal = true;
+        std::stack<std::string> tags;
+        std::getline(std::cin, text);
+        if(text == "#") break;
+        
+        for(int i = 0; i<text.length(); ++i)
+        {
+            auto c = text[i];
+            
+            if(c == '<')
+            {
+                std::string tagname;
+                for(int j = i+1; j<text.length(); ++j)
+                {
+                    if(text[j]=='>')
+                    {
+                        //</tag>
+                        if((text.length() > i + 1) && text[i+1]=='/')
+                        {
+                            tagname = std::string(text.begin() + i+2, text.begin() + j);
+                            //std::cout << "[TEST] tag: " << tagname << std::endl;
+                            
+                            if((tags.empty() == false) && (tagname == tags.top()))
+                                tags.pop();
+                            else
+                                isLegal = false;
+                        }
+                        else
+                        {
+                            
+                            // not <br />
+                            if(text[j-1] != '/')
+                            {
+                                //<a locat = ">
+                                if((text.length() > i + 2 )&& text[i+2] == ' ')
+                                {
+                                    tagname = std::string(text.begin() + i+1, text.begin() + 2);
+                                }
+                                else
+                                    tagname = std::string(text.begin() + i+1, text.begin() + j);
+                                
+                                
+                               // std::cout << "[TEST] tag: " << tagname << std::endl;
+                                tags.push(tagname);
+                            }
+                            
+                        }
+                        break;
+                    }
+                }
+                
+                if(isLegal == false)
+                    break;
+                
+            }
+            
+        }
+        if(tags.empty() == false) isLegal = false;
+
+        if(isLegal)
+            std::cout << "legal\n";
+        else
+            std::cout <<"illegal\n";
+        
+    }//while
+    return 0;
+}
